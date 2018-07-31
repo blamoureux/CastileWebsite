32fae3d64c9b6dd051713e40b7a6a47a84864823
Robert Lamoureux <blamoureux79@gmail.com>
2018-07-30 20:23:41 -0700

    Full commit

--- public/scripts/homeImage.js
+++ public/scripts/homeImage.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 var theImages = new Array() // do not change this
 
-theImages[0] = 'images/home/focusImage1.jpg'
-theImages[1] = 'images/home/focusImage2.jpg'
-theImages[2] = 'images/home/focusImage3.jpg'
-theImages[3] = 'images/home/focusImage4.jpg'
+theImages[0] = '/images/home/focusImage6a.jpg'
+theImages[1] = '/images/home/focusImage7a.jpg'
+theImages[2] = '/images/home/focusImage8a.jpg'
+theImages[3] = '/images/home/focusImage8a.jpg'
 
 // do not edit anything below this line
 
@@ -24,9 +24,9 @@ document.write('<img src="'+theImages[whichImage]+'">');
 
 var quoteImages = new Array() // do not change this
 
-quoteImages[0] = 'images/home/HPquote1.jpg'
-quoteImages[1] = 'images/home/HPquote2.jpg'
-quoteImages[2] = 'images/home/HPquote3.jpg'
+quoteImages[0] = '/images/home/HPquote1.jpg'
+quoteImages[1] = '/images/home/HPquote2.jpg'
+quoteImages[2] = '/images/home/HPquote3.jpg'
 
 // do not edit anything below this line
 
